08e08b5e950e103b9243ae8e4815c8d83351e28c
Yann Zavattero <yann.zavattero@hotmail.com>
2022-12-20 23:51:32 +0100

    V0.00.000.0.1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,25 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes, UrlSegment } from '@angular/router';
+import { ChoiceComponent } from './core/choice/choice.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    matcher: (url: UrlSegment[]) => {
+      return url.length === 1 && url[0].path.match('^[0-9]*$')
+        ? {
+            consumed: url,
+            posParams: {
+              left: new UrlSegment('toto', {}),
+            },
+          }
+        : null;
+    },
+    component: ChoiceComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/core/choice/choice.component.ts
+++ src/app/core/choice/choice.component.ts
@@ -1,4 +1,5 @@
 import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { IChoice } from '@models/choice';
 
 @Component({
@@ -8,15 +9,38 @@ import { IChoice } from '@models/choice';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ChoiceComponent {
-  @Input() personTarget!: string;
+  @Input() for!: string;
   @Input() leftChoice!: IChoice;
   @Input() rightChoice!: IChoice;
 
   hover: boolean = false;
   transition: boolean = false;
+  displayNext: boolean = false;
+  leftIsSelected?: boolean;
+
+  constructor(private route: ActivatedRoute) {
+    console.log(this.route.snapshot.paramMap.get('left'));
+  }
 
   get isFixed(): boolean {
     return !this.hover && !this.transition;
+  }
+
+  get leftStyle(): { [key: string]: any } {
+    return this.getStyle('left');
+  }
+
+  get rightStyle(): { [key: string]: any } {
+    return this.getStyle('right');
+  }
+
+  next(): void {
+    console.log('toto');
+  }
+
+  select(choice: 'left' | 'right'): void {
+    this.leftIsSelected = choice === 'left';
+    this.displayNext = true;
   }
 
   hoverState(isHover: boolean): void {
@@ -25,5 +49,18 @@ export class ChoiceComponent {
 
   transitionState(isTransition: boolean): void {
     this.transition = isTransition;
+  }
+
+  private getStyle(side: 'left' | 'right'): { [key: string]: any } {
+    if (this.leftIsSelected === undefined) return {};
+
+    const bool = side === 'left' ? !this.leftIsSelected : this.leftIsSelected;
+    return bool
+      ? {
+          width: 0,
+          flexGrow: 0,
+          overflow: 'hidden',
+        }
+      : {};
   }
 }
